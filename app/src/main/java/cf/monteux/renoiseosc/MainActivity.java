cb20f910d26154674ce782ca1b69d747dca1ad30
Rove Monteux <rmonteux@centrum.cz>
2018-01-17 10:40:26 +0100

    Added menu.

--- app/src/main/java/cf/monteux/renoiseosc/MainActivity.java
+++ app/src/main/java/cf/monteux/renoiseosc/MainActivity.java
@@ -17,14 +17,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_mixer:
+                    mTextMessage.setText(R.string.title_mixer);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_effects:
+                    mTextMessage.setText(R.string.title_effects);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_piano:
+                    mTextMessage.setText(R.string.title_piano);
+                    return true;
+                case R.id.navigation_configuration:
+                    mTextMessage.setText(R.string.title_configuration);
                     return true;
             }
             return false;
